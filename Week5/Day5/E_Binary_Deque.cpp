5b180bd88c30b2b5692acd44fcb51a08426d943e
Md. Mahmud Hasan Khandker <166143202+mahmud-11@users.noreply.github.com>
2024-11-28 01:34:03 +0600

    Add files via upload

---
+++ Week5/Day5/E_Binary_Deque.cpp
@@ -0,0 +1,58 @@
+//  week4 practice problem 7 Accepted
+// E. Binary Deque
+
+#include <bits/stdc++.h>
+#define ll long long
+#define yes "YES\n"
+#define no "NO\n"
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, s;
+        cin >> n >> s;
+        vector<int> a(n);
+        int total_sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            total_sum += a[i];
+        }
+
+        if (total_sum < s)
+        {
+            cout << -1 << '\n';
+            continue;
+        }
+
+        int ans = 0, sum = 0;
+        int l = 0, r = 0;
+        int cnt_0 = 0;
+        while (r < n)
+        {
+            sum += a[r];
+            if (sum == s)
+            {
+                ans = max(ans, r - l + 1);
+            }
+            else
+            {
+                while (sum > s && l <= r)
+                {
+                    sum -= a[l];
+                    l++;
+                }
+            }
+            r++;
+        }
+        cout << n - ans << '\n';
+    }
+
+    return 0;
+}
