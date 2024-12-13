808db23a3fc58cbb39676db094df20f2e0a46f30
Md. Mahmud Hasan Khandker <166143202+mahmud-11@users.noreply.github.com>
2024-12-13 23:54:42 +0600

    Add files via upload

---
+++ Week6/Day6/B_Petr_and_a_Combination_Lock.cpp
@@ -0,0 +1,46 @@
+// week6 topicwise problem 1 Acepted
+// B. Petr and a Combination Lock
+
+#include <bits/stdc++.h>
+#define ll long long
+#define yes "YES\n"
+#define no "NO\n"
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    bool flag = false;
+    for (int mask = 0; mask < (1 << n); mask++)
+    {
+        int sum = 0;
+        for (int k = 0; k < n; k++)
+        {
+            if ((mask >> k) & 1)
+                sum += a[k];
+            else
+                sum -= a[k];
+        }
+        if (sum % 360 == 0)
+        {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag)
+        cout << yes;
+    else
+        cout << no;
+
+    return 0;
+}
